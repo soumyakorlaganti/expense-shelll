321a11dbb53269f5411aaabb48cb708719eb5c98
soumya <korlagani.soumya@gmail.com>
2025-05-16 06:06:24 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -37,5 +37,15 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#VALIDATE $? "Setting up root password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.soumyadevops.space -uroot -pExpenseApp@1 -e 'show datatypes;' &>>LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
