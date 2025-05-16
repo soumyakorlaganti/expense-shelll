38110edd7ba316cb4513e903c1ed4d7952e934eb
soumya <korlagani.soumya@gmail.com>
2025-05-16 12:26:43 +0530

    hello script

--- backend.sh
+++ backend.sh
@@ -36,5 +36,11 @@ VALIDATE $? "Enabling nodejs:20 version"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-useradd expense
-VALIDATE $? "Creating expense user"
+id expense
+if [ $? -ne 0 ]
+then
+    useradd expense -y &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
