362d5c65be2ba984f700b29ba2f8c32fa997942a
soumya <korlagani.soumya@gmail.com>
2025-05-16 11:21:43 +0530

    hello script

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -41,10 +43,10 @@ VALIDATE $? "Starting MySQL Server"
 #VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h db.soumyadevops.space -uroot -pExpenseApp@1 -e 'show datatypes;' &>>$LOGFILE
+mysql -h db.soumyadevops.space -uroot -p${mysql_root_password} -e 'show datatypes;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
